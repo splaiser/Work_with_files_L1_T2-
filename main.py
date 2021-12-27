cb205cd9795f93e86267f3cc63040772bd9c7242
splaiser <76589075+splaiser@users.noreply.github.com>
2021-12-27 10:08:12 +0300

    fix

--- main.py
+++ main.py
@@ -1,68 +1,27 @@
-def get_sorted():
-    count_file_1 = sum(1 for line in open('1.txt', 'r', encoding='UTF-8'))
-    count_file_2 = sum(1 for line in open('2.txt', 'r', encoding='UTF-8'))
-    count_file_3 = sum(1 for line in open('3.txt', 'r', encoding='UTF-8'))
-    if count_file_1 < count_file_2 < count_file_3:
-        with open('1.txt' , 'r', encoding='UTF-8') as file_1, \
-                open('2.txt' , 'r', encoding='UTF-8') as file_2, \
-                open('3.txt', 'r', encoding='UTF-8') as file_3, \
-                open('final.txt', 'w', encoding='UTF-8') as file_f:
-            file_f.write('1.txt\n')
-            file_f.write(str(count_file_1) + '\n')
-            file_f.write(file_1.read() + '\n')
-            file_f.write('2.txt\n')
-            file_f.write(str(count_file_2) + '\n')
-            file_f.write(file_2.read() + '\n')
-            file_f.write('3.txt\n')
-            file_f.write(str(count_file_3) + '\n')
-            file_f.write(file_3.read() + '\n')
+from pathlib import Path
+import sys, os
 
-    elif count_file_2 < count_file_3 < count_file_1:
-        with open('1.txt' , 'r', encoding='UTF-8') as file_1, \
-                open('2.txt' , 'r', encoding='UTF-8') as file_2, \
-                open('3.txt', 'r', encoding='UTF-8') as file_3, \
-                open('final.txt', 'w', encoding='UTF-8') as file_f:
-            file_f.write('2.txt\n')
-            file_f.write(str(count_file_2) + '\n')
-            file_f.write(file_2.read() + '\n')
-            file_f.write('3.txt\n')
-            file_f.write(str(count_file_3) + '\n')
-            file_f.write(file_3.read() + '\n')
-            file_f.write('1.txt\n')
-            file_f.write(str(count_file_1) + '\n')
-            file_f.write(file_1.read() + '\n')
+source_dir = Path('D:/Python/Netology/work_with_files/lesson_1_task_2/')
+files = source_dir.glob('*.txt')
+file_list = []
 
 
-    elif count_file_3 < count_file_2 < count_file_1:
-        with open('1.txt' , 'r', encoding='UTF-8') as file_1, \
-                open('2.txt' , 'r', encoding='UTF-8') as file_2, \
-                open('3.txt', 'r', encoding='UTF-8') as file_3, \
-                open('final.txt', 'w', encoding='UTF-8') as file_f:
-            file_f.write('3.txt\n')
-            file_f.write(str(count_file_3) + '\n')
-            file_f.write(file_3.read() + '\n')
-            file_f.write('2.txt\n')
-            file_f.write(str(count_file_2) + '\n')
-            file_f.write(file_2.read() + '\n')
-            file_f.write('1.txt\n')
-            file_f.write(str(count_file_1) + '\n')
-            file_f.write(file_1.read() + '\n')
-
-    elif count_file_2 < count_file_1 < count_file_3:
-        with open('1.txt', 'r', encoding='UTF-8') as file_1, \
-                open('2.txt', 'r', encoding='UTF-8') as file_2, \
-                open('3.txt', 'r', encoding='UTF-8') as file_3, \
-                open('final.txt', 'w', encoding='UTF-8') as file_f:
-            file_f.write('2.txt\n')
-            file_f.write(str(count_file_2) + '\n')
-            file_f.write(file_2.read() + '\n')
-            file_f.write('1.txt\n')
-            file_f.write(str(count_file_1) + '\n')
-            file_f.write(file_1.read() + '\n')
-            file_f.write('3.txt\n')
-            file_f.write(str(count_file_3) + '\n')
-            file_f.write(file_3.read() + '\n')
+def get_files(files):
+    for file in files:
+        with file.open('r', encoding='UTF-8') as file_handle:
+            lenght_file = sum(1 for line in open(file, 'r', encoding='UTF-8'))
+            filename = os.path.basename(file)
+            file_list.append({'filename': filename, 'lenght': lenght_file, 'text': file_handle.read()})
 
 
+def get_result():
+    get_files(files)
+    with open('result', 'w', encoding='UTF-8') as res:
+        file_list.sort(key=lambda file_list: file_list['lenght'])
+        for dict in file_list:
+            res.write(str(dict.get('filename') + '\n'))
+            res.write(str(dict.get('lenght')) + '\n')
+            res.write(str(dict.get('text') + '\n'))
 
-get_sorted()
+
+get_result()
